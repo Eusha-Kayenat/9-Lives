95bd11bb8bcdb80eaf0bb365658e132c0b052720
Afnan Mohammad Hafiz <afnan1075@gmail.com>
2026-09-09 01:11:25 +0600

    added game icon for direct play

---
+++ 9 Lives Game/Play 9 Lives.bat
@@ -0,0 +1,9 @@
+@echo off
+title 9 Lives: Evil Cat World
+cd /d "%~dp0"
+if exist "dist\9 Lives\9 Lives.exe" (
+    start "" "dist\9 Lives\9 Lives.exe"
+    exit
+)
+start "" python "9 Lives.py"
+exit
